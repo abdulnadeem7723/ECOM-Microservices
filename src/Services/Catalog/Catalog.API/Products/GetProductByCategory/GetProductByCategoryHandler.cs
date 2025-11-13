f996e82bae43fc4cd6e5de57a4d85999b506d313
Abdul Nadeem <abdulnadeem7723@gmail.com>
2025-11-13 18:08:49 +0530

    GetProductByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Threading.Tasks;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            var product = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync();
+            return new GetProductByCategoryResult(product);
+        }
+    }
+}
