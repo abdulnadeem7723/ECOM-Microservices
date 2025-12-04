7259f7e8163b6ff2e13b5c2ae0f8a9f43c59aa1b
Abdul Nadeem <abdulnadeem7723@gmail.com>
2025-12-04 11:57:41 +0530

    grpc microservice

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,26 +1,61 @@
-﻿
+﻿using Mapster;
+using Discount.Grpc.Data;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Discount.Grpc.Models;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger): DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            //TODO: GetDiscount from Database
-            return base.GetDiscount(request, context);
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+            if (coupon == null)
+            {
+                coupon = new Models.Coupon { ProductName = "No Discount", Description = "No discount", Amount = 0 };
+            }
+            logger.LogInformation("Discount is retrieved for productname: {productname}", request.ProductName);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request."));
+            dbContext.Coupons.Add(coupon);
+            
+            await dbContext.SaveChangesAsync();
+            
+            logger.LogInformation("Discount is successfully created for productname: {productname}", request.Coupon.ProductName);
+            return coupon.Adapt<CouponModel>();
         }
-        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
         {
-            return base.UpdateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request."));
+            dbContext.Coupons.Update(coupon);
+
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount is successfully updated for productname: {productname}", request.Coupon.ProductName);
+            return coupon.Adapt<CouponModel>();
         }
-        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
         {
-            return base.DeleteDiscount(request, context);
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with porductname:{request.ProductName} was not found."));
+            }
+            
+
+            dbContext.Remove(coupon);
+            await dbContext.SaveChangesAsync();
+            logger.LogInformation("Discount is successfully deleted for productname: {productname}", request.ProductName);
+            return new DeleteDiscountResponse { Success=true} ;
         }
     }
 }
